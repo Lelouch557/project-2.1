4a5247eda53c05f7ca531d2f1a88e1246a226d49
marco <m.groenhagen@st.hanze.nl>
2019-11-05 20:36:47 +0100

    working graph

--- main.py
+++ main.py
@@ -7,54 +7,69 @@ from serial import *
 
 class Plot:
 
-    def __init__(self):
+    def __init__(self, color):
         self.s = 1
         self.max = 100
         self.min = 0
         self.x2 = 50
         self.y2 = 550 - 5*randint(self.min, self.max)
-        self.paused = False
+        self.set = True
+        self.color = color
 
     def step(self, canvas):
-        if not self.paused:
+        pass
+
+class TemperaturePlot(Plot):
+
+    def __init__(self, color):
+        super().__init__(color)
+
+    def step(self, canvas):
+        if self.set:
             if self.s == 23:
                 # new frame
                 self.s = 1
                 self.x2 = 50
-                canvas.delete('temp')  # only delete items tagged as temp
-            canvas.delete('bounds')
+                canvas.delete('temp1')  # only delete items tagged as temp
             x1 = self.x2
             y1 = self.y2
             self.x2 = 50 + self.s * 50
             self.y2 = 550-5*randint(self.min, self.max)
-            canvas.create_line(x1, y1, self.x2, self.y2, fill='blue', tags='temp')
-            min_line = canvas.create_line(50, 550, 1150, 550, width=3, fill='red',tags='bounds')
-            max_line = canvas.create_line(50, 50, 1150, 50, width=3, fill='red',tags='bounds')
-            canvas.coords(max_line, 50, 550 - 5 * self.max, 1150, 550 - 5 * self.max)
-            canvas.coords(min_line, 50, 550 - 5 * self.min, 1150, 550 - 5 * self.min)
+            canvas.create_line(x1, y1, self.x2, self.y2, fill=self.color, tags='temp1')
             self.s += 1
+        else:
+            canvas.delete('temp1')
+            self.s = 1
+            self.x2 = 50
         canvas.after(300, self.step, canvas)
 
+class BrightnessPlot(Plot):
 
-    def setEntry(self, mini, maxi):
-        if mini >= 0 and maxi <= 100 and mini <= maxi:
-            self.minEntry = mini
-            self.maxEntry = maxi
-            self.getrange()
+    def __init__(self, color):
+        super().__init__(color)
+
+    def step(self, canvas):
+        if self.set:
+            if self.s == 23:
+                # new frame
+                self.s = 1
+                self.x2 = 50
+                canvas.delete('temp2')  # only delete items tagged as temp
+            x1 = self.x2
+            y1 = self.y2
+            self.x2 = 50 + self.s * 50
+            self.y2 = 550-5*randint(self.min, self.max)
+            canvas.create_line(x1, y1, self.x2, self.y2, fill=self.color, tags='temp2')
+            self.s += 1
         else:
-            print("Invalid range")
-
-    def getrange(self):
-        self.min = self.minEntry
-        self.max = self.maxEntry
-
-    def pause(self):
-        if self.paused:
-            self.paused = False
-        else:
-            self.paused = True
+            canvas.delete('temp2')
+            self.s = 1
+            self.x2 = 50
+        canvas.after(300, self.step, canvas)
 
 max_length = 150
+programmed_length = 150
+
 def open_settings():
     settings = Tk()
     settings.geometry("300x100")
@@ -67,23 +82,31 @@ def open_settings():
 
 
 def set_length(window, val):
-    global max_length
+    global max_length, programmed_length
     for char in val:
-        if char in ascii_letters:
+        if char in ascii_letters or char in punctuation:
             return windll.user32.MessageBoxW(0, "Please enter a valid length", "Invalid length", 0)
     if val == "":
         return windll.user32.MessageBoxW(0, "Please enter a length.", "Length field is empty", 0)
     else:
         val = int(val)
-        if val > 0:
+        if val > 0 and val <= programmed_length:
             max_length = val
             window.destroy()
         else:
             return windll.user32.MessageBoxW(0, "Please enter a positive length.", "Invalid length", 0)
 
-plot = Plot()
+
+def show_plot(plot1, plot2):
+    temp_plot.set = plot1
+    bright_plot.set = plot2
+
+
+temp_plot = TemperaturePlot('blue')
+bright_plot = BrightnessPlot('red')
+show_plot(True, False)
 root = Tk()
-root.geometry("1350x700")
+root.geometry("1200x700")
 root.title('SunShade by HanzeTech')
 
 tab_master = Notebook(root)
@@ -91,14 +114,14 @@ general_tab = Frame(tab_master)
 tab_master.add(general_tab, text="General")
 canvas = Canvas(general_tab, width=1600, height=600, bg='white')  # 0,0 is top left corner
 canvas.pack(expand=YES, fill=BOTH)
-pause_button = Button(canvas, text='Pause', command=plot.pause)
-pause_button.place(x=1237, y=350)
 settings = Button(canvas, text="Settings...", command=open_settings)
-settings.place(x=1225, y=400)
+settings.place(x=800, y=600)
 
 canvas.create_line(50, 550, 1150, 550, width=2)  # x-axis
 canvas.create_line(50, 550, 50, 50, width=2)  # y-axis
 
+temp_button = Button(canvas, text="Show Temperature", command=lambda: show_plot(True, False)).place(x=400, y=600)
+bright_button = Button(canvas, text="Show Brightness", command=lambda: show_plot(False, True)).place(x=600, y=600)
 # x-axis
 for i in range(23):
     x = 50 + (i * 50)
@@ -107,7 +130,6 @@ for i in range(23):
 xlabel = Label(canvas, text="Step", fg='black', bg='white')
 xlabel.pack()
 canvas.create_window(1150, 575, window=xlabel)
-
 # y-axis
 for i in range(11):
     y = 550 - (i * 50)
@@ -116,6 +138,7 @@ for i in range(11):
 ylabel = Label(canvas, text="Value", fg="black", bg="white")
 ylabel.pack()
 canvas.create_window(25, 25, window=ylabel)
-canvas.after(300, plot.step, canvas)
+canvas.after(300, temp_plot.step, canvas)
+canvas.after(300, bright_plot.step, canvas)
 tab_master.pack(expand=1, fill="both")
 root.mainloop()
