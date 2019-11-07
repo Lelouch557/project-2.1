c049445bf838a64cde55d7902e6b417136e3b635
Lelouch557 <andreblok@hotmail.nl>
2019-11-07 15:02:01 +0100

    Update requestData.py
    request data is now a class

--- python/requestData.py
+++ python/requestData.py
@@ -1,19 +1,36 @@
 from time import sleep
 import serial
 
-ser = serial.Serial()
-ser.baudrate=19200
-ser.port="COM3"
-ser.open()
-print(ser.readline())
-while ser.isOpen():
-    sleep(0.1)
-    print('input?')
-    T = input()
-    if(T == "close"):
-        break
-    T = (T + "\n\r").encode()
-    ser.write(T)
-    print(ser.readline())
-    print("4")
-ser.close()
+class RequestData:
+    def __init__(self, comp):
+        self.ser = serial.Serial()
+        self.ser.baudrate=19200
+        self.ser.port = comp
+        self.ser.open()
+        if(self.ser.isOpen()):
+            self.ser.readline()
+            return "ok"
+        else:
+            return "Can't open connection"
+
+    def encode_string(self, string):
+        ret = (string + "\n\r").encode()
+        return ret
+
+    def temp(self):
+        self.ser.write(self.encode_string("TEMP"))
+
+    def light(self):
+        self.ser.write(self.encode_string("LIGHT"))
+
+    def distance(self):
+        self.ser.write(self.encode_string("DISTANCE"))
+
+    def down(self):
+        self.ser.write(self.encode_string("DOWN"))
+
+    def up(self):
+        self.ser.write(self.encode_string("UP"))
+
+    def close_connection(self):
+        self.ser.close()
