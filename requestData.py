a753ab2f71d46c06b4baca61698c208fc41d58df
Nathan <N.sirnathanmitchel@gmail.com>
2019-11-07 13:21:21 +0100

    push data

--- requestData.py
+++ requestData.py
@@ -4,6 +4,7 @@ import serial
 import shutter
 
 ser = serial.Serial('COM5', 19200, timeout=1, parity=serial.PARITY_EVEN, rtscts=1)
+T = 'TEMP'.encode()
 while True:
     sleep(1)
     getVal = ser.readline()
